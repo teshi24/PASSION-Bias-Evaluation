2fd183297f1d77cf1c7893aa4006f58e12256245
teshi24 <nadja.stadi@gmail.com>
2025-04-30 15:49:14 +0200

    using smaller input size (128 instead of 224)

--- src/trainers/evaluation_trainer.py
+++ src/trainers/evaluation_trainer.py
@@ -104,8 +104,8 @@ class EvaluationTrainer(ABC, object):
         # load the dataset to evaluate on
         self.transform = transforms.Compose(
             [
-                transforms.Resize((256, 256)),
-                transforms.CenterCrop(224),
+                transforms.Resize((144, 144)),
+                transforms.CenterCrop(128),
                 transforms.ToTensor(),
                 transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
             ]
