6d5e3f54fa2b3012449de2721fca2aa2c76a2797
teshi24 <nadja.stadi@gmail.com>
2025-05-01 08:32:08 +0200

    getitem reduced logging

--- src/datasets/passion_dataset.py
+++ src/datasets/passion_dataset.py
@@ -145,9 +145,10 @@ class PASSIONDataset(GenericImageDataset):
         elif self.val_transform and not self.training:
             image = self.val_transform(image)
 
-        logger.debug(f"diagnosis: {int(diagnosis)}")
-        logger.debug(f"img_name: {img_name}")
-        logger.debug(f"index: {index}")
+        if index % 250 == 0:
+            logger.debug(f"diagnosis: {int(diagnosis)}")
+            logger.debug(f"img_name: {img_name}")
+            logger.debug(f"index: {index}")
 
         if self.train_data_only:
             return image, int(diagnosis)
